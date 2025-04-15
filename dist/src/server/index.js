9229ea6d5de7560969e678825c3e5db0ec874af3
katoztmy <kato.eng.dao@gmail.com>
2025-04-15 21:19:00 +0900

    fix nodemon file etc...

--- dist/src/server/index.js
+++ dist/src/server/index.js
@@ -11,16 +11,11 @@ function _interop_require_default(obj) {
 }
 const app = (0, _express.default)();
 const PORT = process.env.PORT || 3000;
-// コンパイル後の__dirnameからプロジェクトルートへの正しいパスを設定
-// dist/server/からプロジェクトルートに移動するには ../.. ではなく ../../.. が必要
-const clientBuildPath = _path.default.join(__dirname, "../../../../client-build");
-// 静的ファイル提供の設定
+const clientBuildPath = _path.default.join(__dirname, "../../../client-build");
 app.use(_express.default.static(clientBuildPath));
-// /self-introductionルートへのアクセス時
-app.get("/self-introduction", (req, res)=>{
+app.get("/self-introduction", (_req, res)=>{
     res.sendFile(_path.default.join(clientBuildPath, "self-introduction/index.html"));
 });
-// ルートへのアクセス時
 app.get("/", (_req, res)=>{
     res.redirect("/self-introduction/");
 });
--- src/server/index.ts
+++ src/server/index.ts
@@ -4,7 +4,7 @@ import path from "path";
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-const clientBuildPath = path.join(__dirname, "../../../../client-build");
+const clientBuildPath = path.join(__dirname, "../../../client-build");
 
 app.use(express.static(clientBuildPath));
 
